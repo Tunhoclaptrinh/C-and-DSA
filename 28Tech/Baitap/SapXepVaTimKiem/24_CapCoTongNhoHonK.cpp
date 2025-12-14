089ff1035d67996f1697df3d1b38f8f652fd794d
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2025-12-14 23:14:09 +0700

    24_CapSoTongNhoHonK, 25_CapSoTongLonHonK

---
+++ 28Tech/Baitap/SapXepVaTimKiem/24_CapCoTongNhoHonK.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+      cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+      long long need = k - arr[i];
+      auto it = lower_bound(arr + i + 1, arr + n, k - arr[i]);
+      ans += it - (arr + i + 1);
+    }
+
+    cout << ans << endl;
+  }
+}
