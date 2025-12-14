1ea560b897d4b22fd7ad6029a564beebdb74a20b
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2025-12-14 23:31:15 +0700

    26_TichCuaSoLonNhatMang1VaNhoNhatMang2

---
+++ 28Tech/Baitap/SapXepVaTimKiem/26_TichCuaSoLonNhatMang1VaNhoNhatMang2.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int partitionHoare(int arr[], int l, int r) {
+  int pivot = arr[(l + r) / 2];
+  int i = l - 1;
+  int j = r + 1;
+
+  while (true) {
+    do {
+      i++;
+    } while (arr[i] < pivot);
+
+    do {
+      j--;
+    } while (arr[j] > pivot);
+
+    if (i >= j) {
+      return j;
+    }
+
+    swap(arr[i], arr[j]);
+  }
+}
+
+void quickSort(int arr[], int l, int r) {
+  if (l >= r)
+    return;
+  int p = partitionHoare(arr, l, r);
+  quickSort(arr, l, p);
+  quickSort(arr, p + 1, r);
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, m;
+    cin >> n >> m;
+    int a[n], b[m];
+    for (int &x : a) {
+      cin >> x;
+    }
+    for (int &x : b) {
+      cin >> x;
+    }
+
+    quickSort(a, 0, n - 1);
+    quickSort(b, 0, m - 1);
+
+    int tich = a[n - 1] * b[0];
+
+    cout << tich << endl;
+  }
+
+  return 0;
+}
